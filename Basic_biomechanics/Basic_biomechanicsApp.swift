bb8566ccad118ad2a85d179debcb10f7dc2fd156
irgigi <79409202+irgigi@users.noreply.github.com>
2024-01-01 12:35:06 +0500

    Initial Commit

---
+++ Basic_biomechanics/Basic_biomechanicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Basic_biomechanicsApp.swift
+//  Basic_biomechanics
+//
+//  Created by Мамуля on 01.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Basic_biomechanicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
